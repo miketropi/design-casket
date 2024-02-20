251fcb9fd39d427d917bd50104242ee2e8a0a961
Mike <mike.beplus@gmail.com>
2024-02-20 14:05:22 +0700

    First commit

---
+++ design-casket.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+ * Plugin Name: Design Casket
+ * Plugin URI: #
+ * Description: Design your own casket
+ * Author: Beplus
+ * Author URI: #
+ * Version: 1.0.1
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+{
+  /**
+   * Define
+   */
+  define('DC_VERSION', '1.0.1');
+  define('DC_URI', plugin_dir_url(__FILE__));
+  define('DC_DIR', plugin_dir_path(__FILE__));
+}
+
+{
+  /**
+   * Inc
+   */
+  require(DC_DIR . '/inc/static.php');
+  require(DC_DIR . '/inc/hooks.php');
+  require(DC_DIR . '/inc/helpers.php');
+  require(DC_DIR . '/inc/ajax.php');
+  require(DC_DIR . '/inc/template-tags.php');
+}
+
+{
+  /**
+   * Boot
+   */
+}
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+const mix = require('laravel-mix');
+
+mix
+  .js('./src/main.js', 'dist/design-casket.bundle.js')
+  .react()
+  .sass('./src/scss/main.scss', 'css/design-casket.bundle.css')
+  .setPublicPath('dist')
