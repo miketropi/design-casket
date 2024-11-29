28f6ff8213598dc3300a61808bae7149d76f85ca
Mike <mike.beplus@gmail.com>
2024-11-29 16:17:50 +0700

    Update

--- inc/hooks.php
+++ inc/hooks.php
@@ -5,11 +5,12 @@
 
 function dc_send_mail_admin_after_submission_successful ($submissionData, $postID) {
   $to = get_field('email_admin', 'option');
+  $root_site = get_field('root_url_sharing', 'option');
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $message = "Hello,
 Please click the link below to view the proposed coffin design.
 
-<a href='http://develop.local/#designcasket_". $postID ."'>http://develop.local/#designcasket_". $postID ."</a>
+<a href='". $root_site ."/#designcasket_". $postID ."'>". $root_site ."/#designcasket_". $postID ."</a>
 
 Thank you.";
   wp_mail($to, 'Proposed Coffin Design', $message, $headers);
